eb895f3189867be2697ed8dd60116b1a26cfeef1
Antoine Lambert <anlambert@softwareheritage.org>
2026-04-08 11:29:18 +0200

    client/view: Add support for starlette 1.0.0
    Signature of Jinja2Templates.TemplateResponse has changed
    in starlette 1.0.0.

--- swh/graphql/client/view.py
+++ swh/graphql/client/view.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023  The Software Heritage developers
+# Copyright (C) 2023-2026  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -18,5 +18,5 @@ async def explorer_page(request):
 
     templates = Jinja2Templates(directory=os.path.dirname(__file__))
     return templates.TemplateResponse(
-        "explorer.html", {"request": request, "auth": auth}
+        request, "explorer.html", {"request": request, "auth": auth}
     )
